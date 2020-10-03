85db40d57df46ad05ed5f1e8a1e7010447861935
Valkirin <valkirinn@gmail.com>
2020-10-03 03:15:18 +0300

    redux init

---
+++ redux.js
@@ -0,0 +1,42 @@
+const redux = require('redux')
+
+const initialState = {
+  counter: 0,
+}
+
+//Reduser
+const reducer = (state = initialState, action) => {
+  if (action.type === 'ADD') {
+    return {
+      counter: state.counter + 1,
+    }
+  }
+  if (action.type === 'SUB') {
+    return {
+      counter: state.counter - 1,
+    }
+  }
+  if (action.type === 'ADD_NUM') {
+    return {
+      counter: state.counter + action.value,
+    }
+  }
+  return state
+}
+
+//Store
+const store = redux.createStore(reducer)
+store.subscribe(() => {
+  console.log('Subscribe', store.getState())
+})
+
+//Actions
+const addCounter = {
+  type: 'ADD',
+}
+
+store.dispatch(addCounter)
+
+store.dispatch({ type: 'SUB' })
+
+store.dispatch({ type: 'ADD_NUM', value: 10 })
--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import usersReducer from '../features/users/usersSlice'
-import postsReducer from '../features/posts/postsSlice'
-import commentsReducer from '../features/comments/commentsSlice'
+import counterReducer from '../features/counter/counterSlice'
 
 export default configureStore({
   reducer: {
-    users: usersReducer,
-    posts: postsReducer,
-    comments: commentsReducer,
+    counter: counterReducer,
   },
 })
